f0ee54ed4f3b50e9198210d9e21568fb168e6f29
Philip-jl <94510639+Philip-jl@users.noreply.github.com>
2021-12-05 13:45:15 -0800

    Create ColorFinder.py

--- ColorFinder.py
+++ ColorFinder.py
@@ -2,34 +2,19 @@ import cv2 as cv
 import numpy as np
 import argparse as arg
 
-img = cv.imread('Photos/test3pix.jpg') 
+img = cv.imread('Photos/color1.jpg') 
 blur = cv.GaussianBlur(img, (9,9), cv.BORDER_DEFAULT)
 cv.imshow('color',blur)
 
-
-#blank = np.zero(img.shape[:2], dtype=)
 b,g,r =cv.split(img)
 
-#cv.imshow('blue',b)
-#cv.imshow('green',g)
-#cv.imshow('red',r)
-
-#ap=arg.ArgumentParser()
-#ap.add_argument("-i","--image", help = "path to the image")
-#args = vars(ap.parse_args())
-
-#image = cv.imread("Photos/color1.jpg")
-#color = int(image[20,20])
-#print(color)
-
-
+ba=np.round(np.average(b),0)
+ga=np.round(np.average(g),0)
+ra=np.round(np.average(r),0)
 
 print(img.shape)
-print(b)
-print(g)
-print(r)
-#rgb=cv.cvtColor(img, cv.COLOR_BGR2RGB)
-#cv.imshow('RGB', rgb)
-
+print('b =',ba)
+print('g =',ga)
+print('r =',ra)
 
 cv.waitKey(0)
