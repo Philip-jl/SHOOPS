d1bcb59801a3f0e4c1b5e93a2271bf203e54dff5
Philip-jl <94510639+Philip-jl@users.noreply.github.com>
2022-03-29 10:12:47 -0700

    Update MainCD.py

--- MainCD.py
+++ MainCD.py
@@ -3,16 +3,16 @@ from re import A
 import cv2 as cv
 import numpy as np
 import argparse as arg
-import MainSS 
-import Aluminium as Alum
+import MainSS.py   
 import Bromine as Brom
 import Copper as Copp
 import Iron
 import Lead as Lead
 import Mercury as Merc
 import Ph 
-import Sulfate as Sulf
 
-List_of_Values = [Ph.Ph_value]#,Iron.Iron_value,Lead.Lead_value,Copp.Copp_value,Alum.Alum_value,Merc.Merc_value,Sulf.Sulf_value]
+
+
+List_of_Values = [Ph.Ph_value,Iron.Iron_value,Lead.Lead_value,Copp.Copp_value,Merc.Merc_value,Brom.Brom_value]
 
 print(Ph.Ph_value)
